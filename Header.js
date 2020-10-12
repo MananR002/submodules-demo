14e42500b2e4324c4a5bb1012ef0fda59862f1a4
MananR002 <70526665+MananR002@users.noreply.github.com>
2020-10-12 21:35:38 +0530

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Header = (props) => {
-    console.log("Hello Header!")
+    console.log("Hello This is header here!")
     return (
         <div style = {{color:"red"}}>
             <p>{props.name}</p>
