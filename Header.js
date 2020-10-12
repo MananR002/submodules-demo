3d1f0426fceae7f55365414980a85ef5d841927f
manan-solutelabs <manan.r@solutelabs.com>
2020-10-12 18:23:46 +0530

    components added

---
+++ Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    console.log("Hello")
+    return (
+        <div style = {{color:"red"}}>
+            Hello
+        </div>
+    )
+}
+export default Header;
